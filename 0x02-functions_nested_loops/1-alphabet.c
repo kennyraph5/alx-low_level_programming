b5c4846ee436298948bc756af17be3c1ecf3bd1d
kennyraph5 <kennyraph5@gmail.com>
2022-09-14 17:05:08 -0700

    alpha and omega

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet - print alphabet of letters
+ * print_alphabet - function to print abc
  *
- * Return: void
+ * Return: 0
  */
 void print_alphabet(void)
 {
-        int i;
-       for (i = 'a'; i <= 'z'; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
 }
+
