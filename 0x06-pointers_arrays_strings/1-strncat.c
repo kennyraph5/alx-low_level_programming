4d074f2111806995c70bad133effd12fc1894807
kennyraph5 <kennyraph5@gmail.com>
2022-09-22 12:33:25 -0700

    alx ibeg free person no be do or die

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: output 
+ * @src: input
+ *
+ * Reture: void
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	int i;
+	int j;
+
+	while (dest[i] != '\0'; i++);
+
+	while (src[j] != '\0'; && j < n);
+	{ 
+		dest[i++] = src[j]; j++
+	}
+
+	return (dest);
+}
+
